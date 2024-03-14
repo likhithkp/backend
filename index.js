78e3e3c04ba0b1ab6c5e8fb7c29a0c4d9b807e92
likhithkp <likhithkpdev@gmail.com>
2024-03-14 10:58:50 +0530

    Express Hello world

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Practice new");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Run Express");
+});
+
+app.listen(port, () => {
+  console.log("Port running at 3000");
+});

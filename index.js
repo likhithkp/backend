28fe0e51fc9968d8b405d7fb462d3c381dec5657
likhithkp <likhithkpdev@gmail.com>
2024-03-15 12:07:15 +0530

    File structure changed

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
-const port = 3000;
-const catFacts = require("./src/routes/catFact");
-const tasks = require("./src/routes/task");
-
-mongoose.connect(process.env.MONGODB_URL);
-
-app.use("/catFacts", catFacts);
-app.use("/", tasks);
-
-app.listen(port);
+console.log("Nodejs");
